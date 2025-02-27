47b41b8923406a63db8e6b6bcd6be24e97bce170
dev-Shivam999 <shivamswami982@gmail.com>
2025-02-27 16:36:11 +0530

    refactor: clean up loop logic and commented code for clarity

--- loop/loop.py
+++ loop/loop.py
@@ -1,5 +1,25 @@
 number =[1,-2,1,3,-5,6,-7,-3]
 count=0
-for i in number:
-    # print(i)
-    count+=1 
+# for i in number:
+#     # print(i)
+#     count+=1 
+
+for i in range(10):
+    if i==5-1:
+        continue
+    # print((i+1)*3)
+
+str="lol opi oooyyyye"
+
+# dic={}
+
+# for i in str:
+#      dic[i]=dic.get(i,0)+1
+ 
+
+# for i,j in dic.items():
+#     print(i,j)
+#     if j==1:
+#      print(i)
+#      break
+
