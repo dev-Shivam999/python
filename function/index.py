f092f1c5d8167d562c14db259b275e0c2640719a
dev-Shivam999 <shivamswami982@gmail.com>
2025-02-28 12:21:46 +0530

    feat: add various mathematical functions and example usages

--- function/index.py
+++ function/index.py
@@ -1,6 +1,24 @@
 
-def suqer(num,how):
-    return num**how
+# def suquare(num,how):
+#     return num**how
 
-a=suqer(2,3)
-print(a)
+# a=suquare(100000000,3)
+# print(a)
+
+
+# def sum(a,b):
+#     return a+b
+
+# print(sum(1,2))
+
+# def multi(a,b):
+#     return a*b
+
+# print(multi("2",3))
+
+# def area(ri):
+#     return{"area": 3.14*ri**2,"cir":2*3.14*ri}
+
+# print(area(10))
+
+
