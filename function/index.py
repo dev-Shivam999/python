cec5268b1e134046ee5d7ff750f4c6ff439e30b7
dev-Shivam999 <shivamswami982@gmail.com>
2025-02-28 13:02:21 +0530

    feat: add factorial function and example usage

--- function/index.py
+++ function/index.py
@@ -17,8 +17,59 @@
 # print(multi("2",3))
 
 # def area(ri):
-#     return{"area": 3.14*ri**2,"cir":2*3.14*ri}
+#     area=3.14*ri**2
+#     ci=2*3.14*ri
+#     return area,ci
 
 # print(area(10))
 
 
+
+# def greet(name="lol"):
+#     return "Hello "+name
+
+# print(greet('chutiye'))
+# print(greet())
+
+
+
+# cube=lambda x:x**3
+# print(cube(3))
+
+
+
+# def sumAll(*args):
+#     print(*args)
+#     print(args)
+    # return sum(args)
+    # s=0
+    # for i in args:
+    #     s+=i
+    # return s
+
+# print(sumAll(1,2,3,4,5,6,7,8,9,10))
+# sumAll(1,2,3,4)
+
+
+# def printKwargs(**kwargs):
+#     for name,profection in kwargs.items():
+#         print(f"{name} is a {profection}")
+
+# printKwargs(name="jony",profection="docter",prfection="fb")
+
+
+# def generate(num):
+#     for i in range(num):
+#         yield i
+
+
+# for i in generate(10):
+#     print(i)          
+
+
+def factorial(num):
+    if num==0:
+        return 1
+    return num*factorial(num-1)
+
+print(factorial(5))
