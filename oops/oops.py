b5a9cbc5fa16b5818ffe44cf9991a061c8dffb77
dev-Shivam999 <shivamswami982@gmail.com>
2025-03-03 12:56:02 +0530

    feat: encapsulate brand attribute in Car class and add getter method

--- oops/oops.py
+++ oops/oops.py
@@ -1,14 +1,16 @@
 class Car:
   
   def __init__(self,brand,model):
-    self.brand=brand
+    self.__brand=brand
     self.model=model
     print("Car created")
    
+  def get_brand(self):
+    return self.__brand 
     
 
 
 
 
 my_car=Car("lol",20)
-print(my_car)
+print(my_car.get_brand())
